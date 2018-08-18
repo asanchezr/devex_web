b350ac777c615726e5a9a71f2fd01517fb0e1cda
Andrew Sutherland <sutherlanda@gmail.com>
2018-08-18 15:52:23 -0700

    Update auth scheme

--- modules/core/app/config.js
+++ modules/core/app/config.js
@@ -1,65 +1,5 @@
-import request from 'request';
-import validator from 'validator';
-
 (function (window) {
 	'use strict';
-
-	const cleanUser = (user) => {
-		let safeUserObject;
-		safeUserObject = {
-			_id                     : user._id,
-			orgsAdmin               : user.orgsAdmin,
-			orgsMember              : user.orgsMember,
-			orgsPending             : user.orgsPending,
-			displayName             : validator.escape(user.displayName),
-			provider                : validator.escape(user.provider),
-			username                : validator.escape(user.username),
-			created                 : user.created.toString(),
-			roles                   : user.roles,
-			profileImageURL         : user.profileImageURL,
-			email                   : validator.escape(user.email),
-			lastName                : validator.escape(user.lastName),
-			firstName               : validator.escape(user.firstName),
-			additionalProvidersData : user.additionalProvidersData,
-			government              : user.government,
-			notifyOpportunities     : user.notifyOpportunities,
-			notifyEvents            : user.notifyEvents,
-			notifyBlogs             : user.notifyBlogs,
-			userTitle               : user.userTitle,
-			isDisplayEmail          : user.isDisplayEmail,
-			isDeveloper             : user.isDeveloper,
-			paymentMethod           : user.paymentMethod,
-			phone                   : validator.escape(user.phone),
-			address                 : validator.escape(user.address),
-			businessContactName     : validator.escape(user.businessContactName),
-			businessContactEmail    : validator.escape(user.businessContactEmail),
-			businessContactPhone    : validator.escape(user.businessContactPhone),
-			businessName            : validator.escape(user.businessName),
-			businessAddress         : validator.escape(user.businessAddress),
-			businessAddress2        : validator.escape(user.businessAddress2),
-			businessCity            : validator.escape(user.businessCity),
-			businessProvince        : user.businessProvince,
-			businessCode            : validator.escape(user.businessCode),
-			location                : user.location,
-			description             : validator.escape(user.description),
-			website                 : user.website,
-			skills                  : user.skills,
-			skillsData              : user.skillsData,
-			badges                  : user.badges,
-			capabilities            : user.capabilities,
-			endorsements            : user.endorsements,
-			github                  : user.github,
-			stackOverflow           : user.stackOverflow,
-			stackExchange           : user.stackExchange,
-			linkedIn                : user.linkedIn,
-			isPublicProfile         : user.isPublicProfile,
-			isAutoAdd               : user.isAutoAdd,
-			capabilityDetails 		: user.capabilityDetails,
-			capabilitySkills 		: user.capabilitySkills
-		};
-
-		return safeUserObject;
-	};
 
 	var applicationModuleName = 'mean';
 
@@ -85,65 +25,34 @@ import validator from 'validator';
 
   	window.ApplicationConfiguration = service;
 
-	let url = window.location.search;
-	url = url.replace('?', '');
-	let searchParams = new URLSearchParams(url);
+	// Add a new vertical module
+	function registerModule(moduleName, dependencies) {
+		// Create angular module
+		angular.module(moduleName, dependencies || []);
 
-	let user;
-	let userObj = sessionStorage.getItem('user');
-	if (userObj) {
-		user = JSON.parse(userObj);
+		// Add the module to the AngularJS configuration file
+		angular.module(applicationModuleName).requires.push(moduleName);
 	}
-  	
-	if (user) {
-		window.user = user;
-	}
-	else if (searchParams.has('code')) {
-		const code = searchParams.get('code');
-		request(window.apiUrl + '/api/auth/validate?code=' + code, (error, response, body) => {
 
-			if (response && response.statusCode === 200) {
-				user = JSON.parse(body);
-				user = cleanUser(user);
-				if (user) {
-
-					// store user object in session storage
-					sessionStorage.setItem('user', JSON.stringify(user));
-
-					window.user = user;
-				}
-			}
+	// Angular-ui-notification configuration
+	angular.module('ui-notification').config(['NotificationProvider', function(NotificationProvider) {
+		NotificationProvider.setOptions({
+		delay: 2000,
+		startTop: 20,
+		startRight: 10,
+		verticalSpacing: 20,
+		horizontalSpacing: 20,
+		positionX: 'right',
+		positionY: 'bottom'
 		});
-	}
+	}]);
 
-  // Add a new vertical module
-  function registerModule(moduleName, dependencies) {
-	// Create angular module
-	angular.module(moduleName, dependencies || []);
-
-	// Add the module to the AngularJS configuration file
-	angular.module(applicationModuleName).requires.push(moduleName);
-  }
-
-  // Angular-ui-notification configuration
-  angular.module('ui-notification').config(['NotificationProvider', function(NotificationProvider) {
-	NotificationProvider.setOptions({
-	  delay: 2000,
-	  startTop: 20,
-	  startRight: 10,
-	  verticalSpacing: 20,
-	  horizontalSpacing: 20,
-	  positionX: 'right',
-	  positionY: 'bottom'
-	});
-  }]);
-
-  // Angular idle configuration
-  angular.module('ngIdle').config(['IdleProvider', 'KeepaliveProvider', function(IdleProvider, KeepaliveProvider) {
-	IdleProvider.idle(Number(window.sessionTimeoutWarning));
-	IdleProvider.timeout(Number(window.sessionTimeout));
-	KeepaliveProvider.interval(2);
-  }]);
+	// Angular idle configuration
+	angular.module('ngIdle').config(['IdleProvider', 'KeepaliveProvider', function(IdleProvider, KeepaliveProvider) {
+		IdleProvider.idle(Number(window.sessionTimeoutWarning));
+		IdleProvider.timeout(Number(window.sessionTimeout));
+		KeepaliveProvider.interval(2);
+	}]);
 }(window));
 
 
--- modules/users/services/authentication.client.service.js
+++ modules/users/services/authentication.client.service.js
@@ -1,26 +1,129 @@
+import request from 'request';
+import validator from 'validator';
+
 (function () {
 	'use strict';
 	// Authentication service for user variables
+	const cleanUser = (user) => {
+		let safeUserObject;
+		safeUserObject = {
+			_id                     : user._id,
+			orgsAdmin               : user.orgsAdmin,
+			orgsMember              : user.orgsMember,
+			orgsPending             : user.orgsPending,
+			displayName             : validator.escape(user.displayName),
+			provider                : validator.escape(user.provider),
+			username                : validator.escape(user.username),
+			created                 : user.created.toString(),
+			roles                   : user.roles,
+			profileImageURL         : user.profileImageURL,
+			email                   : validator.escape(user.email),
+			lastName                : validator.escape(user.lastName),
+			firstName               : validator.escape(user.firstName),
+			additionalProvidersData : user.additionalProvidersData,
+			government              : user.government,
+			notifyOpportunities     : user.notifyOpportunities,
+			notifyEvents            : user.notifyEvents,
+			notifyBlogs             : user.notifyBlogs,
+			userTitle               : user.userTitle,
+			isDisplayEmail          : user.isDisplayEmail,
+			isDeveloper             : user.isDeveloper,
+			paymentMethod           : user.paymentMethod,
+			phone                   : validator.escape(user.phone),
+			address                 : validator.escape(user.address),
+			businessContactName     : validator.escape(user.businessContactName),
+			businessContactEmail    : validator.escape(user.businessContactEmail),
+			businessContactPhone    : validator.escape(user.businessContactPhone),
+			businessName            : validator.escape(user.businessName),
+			businessAddress         : validator.escape(user.businessAddress),
+			businessAddress2        : validator.escape(user.businessAddress2),
+			businessCity            : validator.escape(user.businessCity),
+			businessProvince        : user.businessProvince,
+			businessCode            : validator.escape(user.businessCode),
+			location                : user.location,
+			description             : validator.escape(user.description),
+			website                 : user.website,
+			skills                  : user.skills,
+			skillsData              : user.skillsData,
+			badges                  : user.badges,
+			capabilities            : user.capabilities,
+			endorsements            : user.endorsements,
+			github                  : user.github,
+			stackOverflow           : user.stackOverflow,
+			stackExchange           : user.stackExchange,
+			linkedIn                : user.linkedIn,
+			isPublicProfile         : user.isPublicProfile,
+			isAutoAdd               : user.isAutoAdd,
+			capabilityDetails 		: user.capabilityDetails,
+			capabilitySkills 		: user.capabilitySkills
+		};
+
+		return safeUserObject;
+	};
+
 	var Authentication = [
 		'$window',
 		function ($window) {
-			var auth = {
-				user: $window.user,
-				permissions : function () {
-					var isUser     = !!$window.user;
-					var ret        = isUser ? $window.user : {};
-					var isAdmin    = isUser && !!~$window.user.roles.indexOf ('admin');
-					var isGov      = isUser && !!~$window.user.roles.indexOf ('gov');
-					ret.loggedIn   = isUser;
-					ret.isLoggedIn = isUser;
-					ret.isUser     = isUser;
-					ret.isAdmin    = isAdmin;
-					ret.isGov      = isGov;
-					return ret;
-				}
-			};
+
+		var auth = {
+			user: $window.user,
+			permissions : function () {
+				var isUser     = !!$window.user;
+				var ret        = isUser ? $window.user : {};
+				var isAdmin    = isUser && !!~$window.user.roles.indexOf ('admin');
+				var isGov      = isUser && !!~$window.user.roles.indexOf ('gov');
+				ret.loggedIn   = isUser;
+				ret.isLoggedIn = isUser;
+				ret.isUser     = isUser;
+				ret.isAdmin    = isAdmin;
+				ret.isGov      = isGov;
+				return ret;
+			}
+		};
+
+
+		/**
+		 * Attempt to log in if there is a github token present or a user in session storage
+		 */
+		let url = window.location.search;
+		url = url.replace('?', '');
+		let searchParams = new URLSearchParams(url);
+
+		let user;
+		let userObj = sessionStorage.getItem('user');
+		if (userObj) {
+			user = JSON.parse(userObj);
+		}
+
+		if (user) {
+			auth.user = user;
 			return auth;
 		}
+		else if (searchParams.has('code')) {
+			const code = searchParams.get('code');
+			return request(window.apiUrl + '/api/auth/validate?code=' + code, (error, response, body) => {
+
+				if (response && response.statusCode === 200) {
+					user = JSON.parse(body);
+					user = cleanUser(user);
+					if (user) {
+
+						// store user object in session storage
+						sessionStorage.setItem('user', JSON.stringify(user));
+
+						auth.user = user;
+						window.location.href = '/';
+						return auth;
+					}
+				}
+			});
+		}
+		else {
+			return auth;
+		}
+		
+	}
+		
 	];
 
 	angular.module ('users.services').factory('Authentication', Authentication);
